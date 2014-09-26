fa7ca5fd290409378d11a709841e4ff8eb0ddae8
cDensley <cdensley.school@gmail.com>
2014-09-26 13:09:04 -0600

    Added documentation

--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -1,5 +1,9 @@
 package chatbot.model;
-
+/**
+ * The Chatbot model class. Used for checking and manipulating strings.
+ * @author Chandler Densley
+ *@version 1.1 9/26/14
+ */
 public class Chatbot
 {
 
@@ -19,28 +23,44 @@ public class Chatbot
 	
 	/**
 	 * Returns the name of the Chatbot object.
-	 * @return THe current name of the Chatbot.
+	 * @return The current name of the Chatbot.
 	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * Returns the number of times Chatbot has chatted.
+	 * @return Number of times Chatbot has chatted.
+	 */
 	public int getChatCount()
 	{
 		return chatCount;
 	}
 	
+	/**
+	 * Sets the name.
+	 * @param name Name of Chatbot.
+	 */
 	public void setName(String name)
 	{
 		this.name =name;
 	}
 	
+	/**
+	 * Updates the number of times Chatbot has chatted.
+	 */
 	private void updateChatCount()
 	{
 		chatCount++;
 	}
 	
+	/**
+	 * Decides if it's okay to quit based on the user input.
+	 * @param input The input of the user.
+	 * @return Quits.
+	 */
 	public boolean quitChecker(String input)
 	{
 		boolean okToQuit = false;
