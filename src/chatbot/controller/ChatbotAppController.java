4f6ede73f1f0c8f73972804af2301de5a53d9564
cDensley <cdensley.school@gmail.com>
2014-09-26 12:56:07 -0600

    Added quickchecker

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -2,22 +2,26 @@ package chatbot.controller;
 
 import javax.swing.JOptionPane;
 
+import chatbot.model.Chatbot;
 import chatbot.view.ChatbotView;
 
 public class ChatbotAppController
 {
 	private ChatbotView applicationView;
+	private Chatbot mySillyChatbot;
+	
 	
 	public ChatbotAppController()
 	{
 		applicationView = new ChatbotView(this);
+		mySillyChatbot = new Chatbot("Cortana");
 	}
 	
 	public void start()
 	{
 		String result = applicationView.showChatbot("Chandler");
 		
-		if (result.equalsIgnoreCase("exit"))
+		if (mySillyChatbot.quitChecker(result))
 		{
 			quit();
 		}
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -6,12 +6,21 @@ public class Chatbot
 	private String name;
 	private int chatCount;
 	
+	
+	/**
+	 * Creates a Chatbot object with the supplied name and initializes the current number of chats to zero.
+	 * @param name The supplied name for the Chatbot.
+	 */
 	public Chatbot(String name)
 	{
 		this.name = name;
 		chatCount = 0;
 	}
 	
+	/**
+	 * Returns the name of the Chatbot object.
+	 * @return THe current name of the Chatbot.
+	 */
 	public String getName()
 	{
 		return name;
@@ -30,5 +39,17 @@ public class Chatbot
 	private void updateChatCount()
 	{
 		chatCount++;
+	}
+	
+	public boolean quitChecker(String input)
+	{
+		boolean okToQuit = false;
+		
+		if(input.equalsIgnoreCase("Goodbye"))
+		{
+			okToQuit = true;
+		}
+		
+		return okToQuit;
 	}
 }
