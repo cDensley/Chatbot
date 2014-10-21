c21a29e331535f89a1f4a6ca28b33369ae8bb03a
cDensley <cdensley.school@gmail.com>
2014-10-21 12:33:48 -0600

    added frame and panel

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -1,6 +1,7 @@
 package chatbot.controller;
 
 import chatbot.model.Chatbot;
+import chatbot.view.ChatbotFrame;
 import chatbot.view.ChatbotView;
 
 public class ChatbotAppController
@@ -10,11 +11,19 @@ public class ChatbotAppController
 	private String startMessage;
 	private String quitMessage;
 	
+
+	/**
+	 * Reference to the GUI JFrame object for the application.
+	 */
+	private ChatbotFrame appFrame;
 	
 	public ChatbotAppController()
 	{
 		applicationView = new ChatbotView(this);
 		mySillyChatbot = new Chatbot("Cortana");
+		appFrame = new ChatbotFrame(this);
+		startMessage = "Welcome to the " + mySillyChatbot.getName() + " chatbot. What is your name?";
+		quitMessage = "Goodbye cruel user!";
 	}
 	
 	public Chatbot getMySillyChatbot()
@@ -22,16 +31,17 @@ public class ChatbotAppController
 		return mySillyChatbot;
 	}
 	
+	
 	public void start()
 	{
-		String result = applicationView.showChatbotDialog(startMessage);
-		
-		while(!mySillyChatbot.quitChecker(result))
-		{
-			result = mySillyChatbot.processText(result);
-			result = applicationView.showChatbotDialog(result);
-		}
-		quit();
+//		String result = applicationView.showChatbotDialog(startMessage);
+//		
+//		while(!mySillyChatbot.quitChecker(result))
+//		{
+//			result = mySillyChatbot.processText(result);
+//			result = applicationView.showChatbotDialog(result);
+//		}
+//		quit();
 	}
 	
 	private void quit()
